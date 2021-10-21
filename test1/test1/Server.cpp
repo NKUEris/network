0b03f3c87b88b94fdb5a7b964b34dcc23b9adb0f
Ygw <1433104855@qq.com>
2021-10-21 17:24:55 +0800

    修改server

--- test1/test1/Server.cpp
+++ test1/test1/Server.cpp
@@ -3,7 +3,24 @@
 #include<winsock.h>
 #pragma comment(lib,"ws2_32.lib")						//加载ws2_32.lib库
 using namespace std;
-void serverInit();
+
+typedef struct serverClient
+{
+	SOCKET serverSocket;
+	SOCKET clientSocket1;
+	SOCKET clientSocket2;
+}CS;
+//服务端初始化
+void init(CS* cs);
+void createSocket(CS* cs);								//创建套接字
+void threadStart(CS* cs);								//调用线程
+int threadReceive1(void* sc);
+int threadReceive2(void* sc);
+
+char buffer1[1024] = { 0 };
+char buffer2[1024] = { 0 };
+
+
 
 int main()
 {
@@ -104,3 +121,48 @@ int main()
 	}
 
 }
+void init(CS* cs)
+{
+	WSADATA wsaData;														//存放socket
+	WSAStartup(MAKEWORD(2, 2), &wsaData);									//声明socket版本
+	cs->serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (cs->serverSocket == INVALID_SOCKET)
+	{
+		cout << "创建socket失败!!!" << endl;
+		WSACleanup();
+		return;
+	}
+	unsigned port = 3484;													//端口号
+		/* struct SOCKADDR_IN{
+	* sin_family;        //Address Family
+	* sin_port;			//16位端口号
+	* sin_addr;			//32位地址
+	* sin_zero[8];		//不使用
+	*/
+	SOCKADDR_IN	serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);										//服务器监听端口
+	serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+
+	if (SOCKET_ERROR == bind(cs->serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)))
+	{
+		cout << "绑定端口失败!!!" << endl;
+		closesocket(cs->serverSocket);
+		WSACleanup();
+		return;
+	}
+	listen(cs->serverSocket, 5);
+}
+void createSocket(CS* cs)
+{
+	SOCKADDR_IN clientAddr1;
+	SOCKADDR_IN clientAddr2;
+
+	int clientAddr1len = sizeof(clientAddr1);
+	int clientAddr2len = sizeof(clientAddr2);
+	cout << "等待连接..." << endl;
+	cs->clientSocket1 = accept(cs->serverSocket, (SOCKADDR*)&clientAddr1, &clientAddr1len);
+	cs->clientSocket2 = accept(cs->serverSocket, (SOCKADDR*)&clientAddr2, &clientAddr2len);
+	cout << "客户端连接成功。" << endl;
+}
+void threadStart()
