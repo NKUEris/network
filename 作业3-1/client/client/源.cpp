f827609dc993ec1770a3b01a5d9591270b5671dc
YangGuangwen <1433104855@qq.com>
2021-12-03 18:42:56 +0800

    计网3-1

--- 作业3-1/client/client/源.cpp
+++ 作业3-1/client/client/源.cpp
@@ -1,27 +1,29 @@
 #include<iostream>
 #include"Winsock2.h"
+#include<fstream>
 #include<time.h>
 #pragma comment(lib,"ws2_32.lib")
+#pragma warning(disable:4996)
 int seq = 0;
 using namespace std;
 SOCKET clientSock=INVALID_SOCKET;
 SOCKADDR_IN clientAddr;
 SOCKADDR_IN serverAddr;
 int addrlen = sizeof(SOCKADDR);
-#pragma pack(1)
+char temp[10000][1024];
+
 class message
 {
 public:
-	int flag=0x01;															//标志位 0X01 SYN,0X02 ACK, 0X04 FIN, 0X08 文件开始传输，0XF 文件传输结束
-	DWORD DstIP, SrcIP;														//源ip和目标ip
-	u_short DstPort, SrcPort;													//源端口号和目标端口号
-	int msgseq;																//消息序号
-	int ackseq;																//接收ack的消息序号
-	int length;																//定长报文长度
-	int index;																//描述消息条数
-	int check;																//校验和
-	int fill;																//填充
-	char msg[1024];															//用于存储消息内容
+	unsigned short flag=0x01;															// 32标志位 0X01 SYN,0X02 ACK, 0X04 FIN, 0X08 文件开始传输，0XF 文件传输结束
+	DWORD DstIP = inet_addr("127.0.0.1"), SrcIP = inet_addr("127.0.0.1");														//源ip和目标ip64
+	u_short DstPort = 3484, SrcPort = 3483;										//源端口号和目标端口号32
+	int msgseq=0;																//消息序号32
+	int ackseq=0;																//接收ack的消息序号32
+	int length=0;																//定长报文长度32
+	int index=0;																//描述消息条数32
+	unsigned short check=0;																//校验和16
+	char msg[1024] = { '0' };															//用于存储消息内容
 	message()
 	{
 		this->flag = 0;
@@ -35,13 +37,23 @@ public:
 	void set_fin();
 	int get_fin();
 	int checksum();
-	int startfile()
+	bool get_start()
 	{
 		return this->flag & 0x08;
 	}
-	int endfile()
+	void set_sart()
+	{
+		if (!get_start()) flag += 0x08;
+		return;
+	}
+	bool get_endfile()
 	{
 		return this->flag & 0xf;
+	}
+	void set_end()
+	{
+		if (!get_endfile()) flag += 0xf;
+		return;
 	}
 };
 message::message(DWORD DstIP, DWORD SrcIP, short DstPort, short SrcPort)
@@ -70,7 +82,7 @@ int message::get_syn()
 int message::get_ack()
 {
 	//ACK = 010第二位标志位
-	if (this->flag & 0x20)
+	if (this->flag & 0x02)
 		return 1;
 	else return 0;
 }
@@ -82,34 +94,54 @@ void message::set_ack(message Msg)		//对消息的确认
 }
 int message::checksum()				//对校验和进行计算
 {
-	int sum = 0;
+	/*unsigned long checksum = 0;
+	unsigned short* buf = (unsigned short*)this;
+	for (int i = 0; i < 528; i++)
+	{
+		checksum += buf[i];
+		int t = checksum >> 16;
+		checksum = t + (checksum & 0xffff);
+	}
+	if (checksum & 0xffff == (0xffff))
+		return 1;
+	return 0;*/
+	unsigned short sum = 0;
 	u_char* temp = (u_char*)this;
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < 528; i++)
 	{
 		sum += temp[2 * i] << 8 + temp[2 * i + 1];
-		while (sum > 0x10000)					//溢出
+		while (sum >= 0x10000)					//溢出
 		{
-			int cont = sum >> 16;
-			sum += cont;
+			unsigned short t = sum >> 16;//将最高位回滚添加至最低位
+			sum += t;
 		}
 	}
-	if (check + (u_short)sum == 0xffff)return 1;
+	if (check + sum == 0xffff)return 1;
 	else return 0;
 }
 void message::setcheck()		//发送方校验和
 {
-	int sum = 0;
+	/*unsigned long checksum = 0;
+	unsigned short* buf = (unsigned short*)this;
+	for (int i = 0; i < 528; i++)
+	{
+		checksum += buf[i];
+		int t = checksum >> 16;
+		checksum = t + (checksum & 0xffff);
+	}
+	this->check = ~checksum & 0xffff;*/
+	unsigned short sum = 0;
 	u_char* temp = (u_char*)this;
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < 528; i++)
 	{
 		sum += temp[2 * i] << 8 + temp[2 * i + 1];
-		while (sum > 0x10000)					//溢出
+		while (sum >=0x10000)					//溢出
 		{
-			int cont = sum >> 16;
-			sum += cont;
+			unsigned short t = sum >> 16;//将最高位回滚添加至最低位
+			sum += t;
 		}
 	}
-	this->check = ~(u_short)sum;			//按位取反
+	this->check = (unsigned short)(~sum);				//按位取反
 }
 void message::set_fin()
 {
@@ -122,25 +154,18 @@ int message::get_fin()
 		return 1;
 	else return 0;
 }
-#pragma pack()
-int buildConnection()
-{
-	message send = message();
-	message recv = message();
-	send.set_syn();
-	send.msgseq = seq++;
-	if (stopWaitSend(send, recv))return 1;
-	else return 0;
-}
-int disconnect()
-{
-	message send = message();
-	message recv = message();
-	send.set_fin();
-	send.msgseq = seq++;
-	if (stopWaitSend(send, recv))return 1;
-	else return 0;
 
+void Send(message& Msg)
+{
+	Msg.setcheck();
+	Msg.msgseq = seq++;
+	sendto(clientSock, (char*)&Msg, sizeof(message), 0, (SOCKADDR*)&serverAddr, sizeof(SOCKADDR));
+}
+void Recv(message& Msg)
+{
+	memset(Msg.msg, 0, sizeof(Msg.msg));
+	recvfrom(clientSock, (char*)&Msg, sizeof(message), 0, (SOCKADDR*)&serverAddr, (int*)&addrlen);
+	seq = Msg.msgseq;
 }
 bool stopWaitSend(message& send, message recv)
 {
@@ -169,25 +194,104 @@ bool stopWaitSend(message& send, message recv)
 		}
 	}
 }
+int buildConnection()
+{
+	message send = message();
+	message recv = message();
+	send.set_syn();
+	send.msgseq = seq++;
+	if (stopWaitSend(send, recv))return 1;
+	else return 0;
+}
+int disconnect()
+{
+	message send = message();
+	message recv = message();
+	send.set_fin();
+	send.msgseq = seq++;
+	if (stopWaitSend(send, recv))return 1;
+	else return 0;
 
-void Send(message& Msg)
-{
-	Msg.setcheck();
-	Msg.msgseq = seq++;
-	sendto(clientSock, (char*)&Msg, sizeof(message),0,(SOCKADDR*)&serverAddr, sizeof(SOCKADDR));
 }
-void Recv(message& Msg)
-{
-	memset(Msg.msg, 0, sizeof(Msg.msg));
-	recvfrom(clientSock, (char*)&Msg, sizeof(message), 0, (SOCKADDR*)&serverAddr,(int*)&addrlen);
-	seq = Msg.msgseq;
-}
-void Read()
-{
-}
-void sendfile()
-{
 
+int read(char name[32], int &length, int &index)
+{
+	index = 0;
+	length = 0;
+	memset(temp, 0, sizeof(temp));
+	ifstream sendfile(name, ifstream::binary);
+	if (!sendfile)
+	{
+		cout << "文件打开失败...." << endl;
+		return 0;
+	}
+	char file = sendfile.get();
+	while (sendfile) 
+	{
+		temp[index][length % 1024] = file;
+		length++;
+		if (length % 1024 == 0)
+		{
+			index++;
+			length = 0;
+		}
+		file = sendfile.get();
+	}
+	sendfile.close();
+	return 1;
+}
+int sendfile(char name[32])
+{
+	int index = 0;
+	int length = 0;
+	read(name, length, index);
+	clock_t start = clock();
+	message send;
+	send.index = index;
+	send.length = length;
+	for (int i = 0; i < strlen(name); i++)
+	{
+		send.msg[i] = name[i];
+	}
+	send.set_sart();
+	send.msgseq = seq++;
+	message recv;
+	if (!stopWaitSend(send, recv))
+	{
+		cout << "文件发送失败..." << endl;
+		return 0;
+	}
+	for (int i = 0; i <= index; i++)
+	{
+		message end;
+		if (i == index)
+		{
+			end.set_end();
+			for (int j = 0; j < length; j++)
+			{
+				end.msg[j] = temp[index][j];
+			}
+		}
+		else
+		{
+			for (int j = 0; j < 1024; j++)
+			{
+				end.msg[j] = temp[i][j];
+			}	
+		}
+		end.msgseq = seq++;
+		if (!stopWaitSend(end, recv))
+		{
+			cout << "文件发送失败..." << endl;
+			return 0;
+		}
+	}
+	clock_t timeend = clock();
+	double time = ((double)(timeend - start) / CLOCKS_PER_SEC);
+	cout << "传输时间：" << time << "s" << endl;
+	cout<<"吞吐率：" << (double)(index + 1) * sizeof(message) * 8 / time/ 1024 / 1024 << "Mbps" << endl;
+	cout << "文件传输成功！" << endl;
+	return 1;
 }
 int main()
 {
@@ -195,9 +299,48 @@ int main()
 	WSADATA wsaData;
 	WSAStartup(wVersionRequested, &wsaData);
 	clientSock = socket(AF_INET, SOCK_DGRAM, 0);
-	clientAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");					//绑定客户端ip地址
-	clientAddr.sin_family = AF_INET;
-	clientAddr.sin_port = htons(3484);
-	bind(clientSock, (SOCKADDR*)&clientAddr, sizeof(SOCKADDR));
-	
+	serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(3484);
+	char op;
+	cout << "是否需要传输文件？(Y/N)" << endl;
+	while (1)
+	{
+		cin >> op;
+		while (op == 'Y')
+		{
+			cout << "请输入要发送的文件名: " << endl;
+			char name[32];
+			cin >> name;
+			if (buildConnection())
+			{
+				cout << "建立链接成功！" << endl;
+				cout << "准备发送文件" << endl;
+				if (sendfile(name))
+				{
+					cout << "文件发送成功！" << endl;
+					cout << "是否需要继续发送文件？(Y/N)" << endl;
+					cin >> op;
+				}
+				else
+					cout << "文件发送失败！" << endl;
+			}
+			else
+			{
+				cout << "建立链接失败..." << endl;
+				return 0;
+			}
+		}
+		if (disconnect())
+		{
+			cout << "链接已断开！" << endl;
+		}
+		else
+		{
+			cout << "断开链接失败！" << endl;
+		}
+
+		return 0;
+	}
 }
+
--- 作业3-1/server/server/源.cpp
+++ 作业3-1/server/server/源.cpp
@@ -1,27 +1,28 @@
 #include<iostream>
 #include"Winsock2.h"
+#include<fstream>
 #include<time.h>
 #pragma comment(lib,"ws2_32.lib")
+#pragma warning(disable:4996)
 using namespace std;
 SOCKET serverSock = INVALID_SOCKET;
 SOCKADDR_IN clientAddr;
 SOCKADDR_IN serverAddr;
 int addrlen = sizeof(SOCKADDR);
 int seq = 0;
-#pragma pack(1)
+char temp[10000][1024];
 class message
 {
 public:
-	int flag;														//标志位 0X01 SYN,0X02 ACK, 0X04 FIN, 0X08 文件开始传输，0XF 文件传输结束
-	DWORD DstIP, SrcIP;														//源ip和目标ip
-	u_short DstPort, SrcPort;												//源端口号和目标端口号
-	int msgseq;																//消息序号
-	int ackseq;																//接收ack的消息序号
-	int length;																//定长报文长度
-	int index;																//描述消息条数
-	int check;																//校验和
-	int fill;																//填充
-	char msg[1024];															//用于存储消息内容
+	unsigned short flag=0x01;														//标志位 0X01 SYN,0X02 ACK, 0X04 FIN, 0X08 文件开始传输，0XF 文件传输结束
+	DWORD DstIP=inet_addr("127.0.0.1"), SrcIP =inet_addr("127.0.0.1");	//源ip和目标ip
+	u_short DstPort=3484, SrcPort=3483;											//源端口号和目标端口号
+	int msgseq=0;																//消息序号
+	int ackseq=0;																//接收ack的消息序号
+	int length=0;																//定长报文长度
+	int index=0;																//描述消息条数
+	unsigned short check=0;																//校验和															
+	char msg[1024] = {'0'};															//用于存储消息内容
 	message()
 	{
 		this->flag = 0;
@@ -35,6 +36,24 @@ public:
 	int checksum();
 	void set_fin();
 	int get_fin();
+	bool get_start()
+	{
+		return this->flag & 0x08;
+	}
+	void set_sart()
+	{
+		if (!get_start()) flag += 0x08;
+		return;
+	}
+	bool get_endfile()
+	{
+		return this->flag & 0xf;
+	}
+	void set_end()
+	{
+		if (!get_endfile()) flag += 0xf;
+		return;
+	}
 	int startfile()
 	{
 		return this->flag & 0x08;
@@ -71,7 +90,7 @@ int message::get_syn()
 int message::get_ack()
 {
 	//ACK = 010第二位标志位
-	if (this->flag & 0x20)
+	if (this->flag & 0x02)
 		return 1;
 	else return 0;
 }
@@ -83,34 +102,55 @@ void message::set_ack(message Msg)		//对消息的确认
 }
 int message::checksum()				//对校验和进行计算
 {
-	int sum = 0;
-	u_char* temp = (u_char*)this;
-	for (int i = 0; i < 16; i++)
+	unsigned long checksum = 0;
+	unsigned short* buf = (unsigned short*)this;
+	for (int i = 0; i < 528; i++)
 	{
-		sum += temp[2 * i] << 8 + temp[2 * i + 1];
-		while (sum > 0x10000)					//溢出
-		{
-			int cont = sum >> 16;
-			sum += cont;
-		}
+		checksum += buf[i];
+		int t = checksum >> 16;
+		checksum = t + (checksum & 0xffff);
 	}
-	if (check + (u_short)sum == 0xffff)return 1;
-	else return 0;
+	if (checksum & 0xffff == (0xffff))
+		return 1;
+	return 0;
+	//unsigned  sum = 0;
+	//u_char* temp = (u_char*)this;
+	//for (int i = 0; i < 528; i++)
+	//{
+	//	sum += temp[2 * i] << 8 + temp[2 * i + 1];
+	//	while (sum >=0x10000)					//溢出
+	//	{
+	//		unsigned t = sum >> 16;//将最高位回滚添加至最低位
+	//		sum += t;
+	//	}
+	//}
+	//if (check + sum == 0xffff)return 1;
+	//else return 0;
 }
 void message::setcheck()		//发送方校验和
 {
-	int sum = 0;
-	u_char* temp = (u_char*)this;
-	for (int i = 0; i < 16; i++)
+	unsigned long checksum = 0;
+	unsigned short* buf = (unsigned short*)this;
+	for (int i = 0; i < 528; i++)
 	{
-		sum += temp[2 * i] << 8 + temp[2 * i + 1];
-		while (sum > 0x10000)					//溢出
-		{
-			int cont = sum >> 16;
-			sum += cont;
-		}
+		checksum += buf[i];
+		int t = checksum >> 16;
+		checksum = t + (checksum & 0xffff);
 	}
-	this->check = ~(u_short)sum;			//按位取反
+	this->check = ~checksum & 0xffff;
+	//unsigned  sum = 0;
+	//u_char* temp = (u_char*)this;
+	//for (int i = 0; i < 528; i++)
+	//{
+	//	sum += temp[2 * i] << 8 + temp[2 * i + 1];
+	//	while (sum >= 0x10000)					//溢出
+	//	{
+	//		unsigned short t = sum >> 16;//将最高位回滚添加至最低位
+	//		sum += t;
+	//	}
+	//}
+	//this->check = (unsigned short)(~ sum);			//按位取反
+
 }
 void message::set_fin()
 {
@@ -123,7 +163,18 @@ int message::get_fin()
 		return 1;
 	else return 0;
 }
-#pragma pack()
+
+void Recv(message& Msg)
+{
+	memset(Msg.msg, 0, sizeof(Msg.msg));
+	recvfrom(serverSock, (char*)&Msg, sizeof(message), 0, (SOCKADDR*)&clientAddr, (int*)&addrlen);
+}
+void Send(message& Msg)
+{
+	Msg.setcheck();
+	sendto(serverSock, (char*)&Msg, sizeof(message), 0, (SOCKADDR*)&clientAddr, sizeof(SOCKADDR));
+}
+
 int buildConnection(message Msg)
 {
 	message send = message();
@@ -151,6 +202,7 @@ bool stopWaitRecv(message& recv, message send)								//服务端停等机制
 		{
 			send.set_ack(recv);
 			send.msgseq = seq++;
+			Send(send);
 			memset((char*)&send, 0, sizeof(message));
 			return true;
 		}
@@ -158,7 +210,19 @@ bool stopWaitRecv(message& recv, message send)								//服务端停等机制
 	cout << "接收失败" << endl;
 	return false;
 }
-void recvFile(message recv)
+void write(char name[32],int length,int &index)
+{
+	ofstream write(name, ofstream::binary);
+	for (int i = 0; i < index; i++)
+	{
+		for (int j = 0; j < 1024; j++)
+			write << temp[i][j];
+	}
+	for (int j = 0; j < length; j++)
+		write << temp[index][j];
+	write.close();
+}
+int recvFile(message recv)
 {
 	message send= message();
 	send.set_ack(recv);
@@ -166,34 +230,94 @@ void recvFile(message recv)
 	Send(send);
 	int index = recv.index;
 	int length = recv.length;
-	char filename[128];
+	char filename[32];
 	memset(filename, 0, sizeof(filename));
 	for (int i = 0; recv.msg[i]; i++)
 		filename[i] = recv.msg[i];
 	for (int i = 0; i <= index; i++)
 	{
+		message recv, send;
+		memset(temp[i], 0, 1024);
+		if (stopWaitRecv(recv, send))
+		{
+			if (i == index)
+			{
+				for (int j = 0; j < length; j++)
+					temp[i][j] = recv.msg[j];
+			}
+			else
+			{
+				for (int j = 0; j < 1024; j++)
+					temp[i][j] = recv.msg[j];
+			}
+		}
+		else
+		{
+			cout << "接受失败！" << endl;
+			return 0;
+		}
+		if (i == index)
+		{
+			if (!recv.get_endfile())
+			{
+				cout << "消息接收失败！" << endl;
+				return 0;
+			}
+		}
+	}	
+	write(filename, length, index);
+	cout << filename << "文件接收成功" << endl;
+	return 1;
+}
 
-	}
-}
-void Send(message& Msg)
-{
-	Msg.setcheck();
-	sendto(serverSock, (char*)&Msg, sizeof(message), 0, (SOCKADDR*)&serverAddr, sizeof(SOCKADDR));
-}
-void Recv(message& Msg)
-{
-	memset(Msg.msg, 0, sizeof(Msg.msg));
-	recvfrom(serverSock, (char*)&Msg, sizeof(message), 0, (SOCKADDR*)&serverAddr, (int*)&addrlen);
-}
 int main()
 {
 	WORD wVersionRequested = MAKEWORD(2, 2);//套接字库版本号
 	WSADATA wsaData;
 	WSAStartup(wVersionRequested, &wsaData);
 	serverSock = socket(AF_INET, SOCK_DGRAM, 0);
-	clientAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");					//绑定客户端ip地址
-	clientAddr.sin_family = AF_INET;
-	clientAddr.sin_port = htons(3484);
-	bind(serverSock, (SOCKADDR*)&clientAddr, sizeof(SOCKADDR));
+	serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(3484);
+	//clientAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");					//绑定客户端ip地址
+	//clientAddr.sin_family = AF_INET;
+	//clientAddr.sin_port = htons(3483);
+	bind(serverSock, (SOCKADDR*)&serverAddr, sizeof(SOCKADDR));
+	while (1)
+	{
+	
+		message recv;
+		Recv(recv);
+		if (recv.get_syn())
+		{
+			cout << "建立链接..." << endl;
+			if (buildConnection(recv))
+			{
+				
 
+			}
+			else
+			{
+				cout << "链接失败！" << endl;
+			}
+		}
+		else if (recv.get_start())
+		{
+			cout << "接收文件中..." << endl;
+			recvFile(recv);
+		}
+		if (recv.get_fin())
+		{
+			if (disconnect(recv))
+			{
+				seq = 0;
+				cout << "客户端链接已断开，输入exit()退出" << endl;
+				string exit;
+				cin >> exit;
+				if (exit == "exit()")
+					return 0;
+			}
+		}
+	}
+	
 }
